98f457e804b41172c8c88c6bce306d855dc4e4b2
Abhay Jogenipalli <ajogenip@amazon.com>
2025-06-03 13:11:11 -0400

    Updating rest pages on home so tokens usage is limited

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,5 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
 import "./App.css";
-
-function Home() {
-
-  return (
-    <>
-    {/* Add main page components here */}
-    </>
-  );
-}
-
 
 // ----------------------
 // App Component with Routing
@@ -18,7 +8,7 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-50 text-foreground">
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* Can add routes here if needed */}
       </Routes>
     </div>
   );
