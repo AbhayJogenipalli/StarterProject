91881a3f6616bc4068395ba378ad6aad9723e809
Abhay Jogenipalli <ajogenip@amazon.com>
2025-06-03 13:10:35 -0400

    Updating rest pages on home so tokens usage is limited

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,25 +1,10 @@
-import { Link } from "react-router-dom";
-import { NavigationMenu, NavigationMenuList, NavigationMenuItem, NavigationMenuLink } from "@/components/ui/navigation-menu";
+import { NavigationMenu, NavigationMenuList } from "@/components/ui/navigation-menu";
 
 export default function Navbar() {
   return (
     <NavigationMenu className="p-4 border-b bg-white shadow w-full">
       <NavigationMenuList className="flex gap-4">
-        <NavigationMenuItem>
-          <Link to="/">
-            <NavigationMenuLink className="text-blue-600 hover:underline px-4 py-2">Home</NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link to="/about">
-            <NavigationMenuLink className="text-blue-600 hover:underline px-4 py-2">About</NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link to="/example">
-            <NavigationMenuLink className="text-blue-600 hover:underline px-4 py-2">Example</NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
+        {/* Can you navigation items if needed */}
       </NavigationMenuList>
     </NavigationMenu>
   );
